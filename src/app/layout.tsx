bfa99ea36952824579ddff40354d21da45f26092
Tristen Hogue <smileyhogue@users.noreply.github.com>
2023-09-15 21:25:29 -0400

    Update OpenGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ export const metadata: Metadata = {
   title: 'Smoking Aces Stats',
   description: 'Smoking Aces Stats',
   openGraph: {
+    title: 'Smoking Aces Stats',
+    description: 'View your stats across all Smoking Aces servers',
+    url: 'https://stats.twothreexray.com',
     images: 'https://stats.twothreexray.com/smokey.png'
   },
 }
