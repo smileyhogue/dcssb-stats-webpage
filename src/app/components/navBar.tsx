f32656f76c49b7f83084e2a9ae294884c305fac7
Tristen Hogue <smileyhogue@users.noreply.github.com>
2023-09-19 14:11:59 -0400

    Add icons for theme switcher

--- src/app/components/navBar.tsx
+++ src/app/components/navBar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import React from "react";
 import Image from "next/image";
 import { Button } from "@/app/components/ui/button";
-import { Moon, Sun } from "lucide-react";
+import { Moon, Sun, Monitor } from "lucide-react";
 import { useTheme } from "next-themes";
 import {
   DropdownMenu,
@@ -13,13 +13,8 @@ import {
 } from "@/app/components/ui/dropdown-menu";
 import {
   NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
 } from "@/app/components/ui/navigation-menu";
 
 const NavBar: React.FC = () => {
@@ -35,39 +30,51 @@ const NavBar: React.FC = () => {
           </NavigationMenuItem>
           <NavigationMenuItem>
             <Button className="px-3">
-              <Link href="https://discord.gg/9aefybCAt9" rel="noopener noreferrer" target="_blank" >Join our Discord</Link>
+              <Link
+                href="https://discord.gg/9aefybCAt9"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Join our Discord
+              </Link>
             </Button>
           </NavigationMenuItem>
 
           <NavigationMenuItem>
             <Button className="px-3">
-              <Link href="https://tacview.twothreexray.com" rel="noopener noreferrer" target="_blank" >Tacview Files</Link>
+              <Link
+                href="https://tacview.twothreexray.com"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Tacview Files
+              </Link>
             </Button>
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
 
       <div className="flex items-center whitespace-nowrap rounded px-3 py-1.5">
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline" size="icon">
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuItem onClick={() => setTheme("light")}>
-            Light
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("dark")}>
-            Dark
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("system")}>
-            System
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              <Sun /> &nbsp; Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              <Moon /> &nbsp; Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              <Monitor /> &nbsp; System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </nav>
   );
